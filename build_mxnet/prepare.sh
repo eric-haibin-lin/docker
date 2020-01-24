9da915c40e550e37d4cff21bf24d38ca409de3fc
Ubuntu <ubuntu@ip-172-31-48-150.ec2.internal>
2020-01-24 06:19:39 +0000

    fix ar

--- build_mxnet/prepare.sh
+++ build_mxnet/prepare.sh
@@ -1,7 +1,7 @@
 set -ex
 export DOCKER_USER=haibinlin
-export MXNET_COMMIT=3ccd7adc
-export MXNET_REMOTE=https://github.com/eric-haibin-lin/mxnet
+export MXNET_COMMIT=593d5b6a8
+export MXNET_REMOTE=https://github.com/dmlc/mxnet
 
 docker build -t $DOCKER_USER/mxnet:$MXNET_COMMIT \
        --build-arg MXNET_COMMIT=$MXNET_COMMIT \
@@ -13,4 +13,3 @@ echo "cp /build/dist/mxnet_cu100*.whl /docker/Dockerfile"
 
 docker run -v ~/efs/chaokun/docker:/docker \
        -it $DOCKER_USER/mxnet:$MXNET_COMMIT bash
-
